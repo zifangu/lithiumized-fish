bb84cd41486114a909050e58f17488d92da15b99
anniejain24 <dhvanijain@gmail.com>
2025-01-27 11:42:47 -0600

    read_alignment

---
+++ read_alignment.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+def readAlignment(path: str):
+
+    with open(path) as file:
+        content = file.readlines()
+        print(content)
+        df = []
+        for line in content:
+            if line == '\n': 
+                continue
+            if '>' in line: 
+                continue
+            df.append(line)
+    return df
+
+def main():
+    readAlignment('data/ProteinMPNN_sequences.txt')
+
+
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+    main()
